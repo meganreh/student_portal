03f05d0dbacafadac133d40d5cb5bb6b2d94a99a
EricCRodriguez <ecrod12@gmail.com>
2020-01-05 15:51:02 -0600

    git push origin info git commit -m git commit -m

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>Info</h1>
+    <div v-for="student in students">
+      <h2>{{ student.first_name }}</h2>
+    </div>
   </div>
 </template>
 
@@ -10,10 +13,29 @@
 export default {
   data: function() {
     return {
-      message: "Student Portal Fun Time"
+      message: "Student Portal Fun Time",
+      students: [
+        {
+          id: 1,
+          first_name: "Peter",
+          last_name: "Name",
+          email: "peter@email.com",
+          password: "password",
+          phone_number: "123456789",
+          bio: "x",
+          linkedin: "x",
+          twitter: "x",
+          website: "x",
+          resume: "x",
+          github: "x",
+          photo: "x"
+        }
+      ]
     };
   },
+
   created: function() {},
+
   methods: {}
 };
 </script>
