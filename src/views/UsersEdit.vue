2afc515bbf5a760edbe6bacae92b899acf538d8c
MalikQuest <jordan.taylor15@gmail.com>
2020-01-06 20:44:35 -0600

    made edits

--- src/views/UsersEdit.vue
+++ src/views/UsersEdit.vue
@@ -1,34 +1,49 @@
 <template>
   <div class="container">
     <h1>Edit Info</h1>
+
     First Name:
     <input type="text" v-model="user.first_name" />
+    <br />
     Last Name:
     <input type="text" v-model="user.last_name" />
+    <br />
     Email:
     <input type="text" v-model="user.email" />
+    <br />
     Phone Number:
     <input type="text" v-model="user.phone_number" />
-    Bio:
-    <input type="text" v-model="user.bio" />
+    <br />
     LinkedIn:
     <input type="text" v-model="user.linkedin" />
+    <br />
     Twitter:
     <input type="text" v-model="user.twitter" />
+    <br />
     Website:
     <input type="text" v-model="user.website" />
+    <br />
     Resume:
     <input type="text" v-model="user.resume" />
+    <br />
     Github:
     <input type="text" v-model="user.github" />
+    <br />
     Photo:
     <input type="text" v-model="user.photo" />
+    <br />
+    Bio:
+    <textarea cols="50" rows="10" type="text" v-model="user.bio" />
+    <br />
+    <button v-on:click="updateInfo(user)">Submit</button>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
